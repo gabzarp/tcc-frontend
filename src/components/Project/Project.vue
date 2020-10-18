4ce35b691c2ced721e2c395192fd5b5d2ed5e4a6
arthur-teixeira <40871498+arthur-teixeira@users.noreply.github.com>
2020-10-18 19:50:29 -0300

    project page, member card

--- src/components/Project/Project.vue
+++ src/components/Project/Project.vue
@@ -1,41 +1,56 @@
 <template>
-    <div class="container">
-        <div class="row pt-3 justify-content-center">
-            <h4 class="">{{project.name}}</h4>
-        </div>
-        <div class="row pt-3 justify-content-center">
-            <a class="px-3 py-1 mx-1" href="#">
-                <img style="width:40px" class="img-fluid" src="../../assets/trello.png" alt="">
-            </a>                         
-            <a class="px-3 py-1 mx-1" href="#">
-                <img style="width:40px" class="img-fluid" src="../../assets/git.png" alt="">
-            </a>
-            <a class="px-3 py-1 mx-1" href="#">
-                <img style="width:40px" class="img-fluid" src="../../assets/slack.png" alt="">
-            </a>
-        </div>
-        <div class="row">
-            <div class="col-8">
-                <h6 class="text-left">{{ 'Dono' }}</h6>
-            </div>
-            <div class="col-4">
-                <div class="row">
-                </div>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class=" pt-3 justify-content-center text-center mb-5 col-12">
+        <h2>{{ project.name }}</h2>
+        <h6 >{{ "Dono" }}</h6>
+      </div>
     </div>
+    <div class="row">
+      <div class="col-10">
+        <h4>Descrição:</h4>
+        <p>{{project.description}}</p>
+      </div>
+      <div class="pt-3 col-2 px-5">
+      <a class="m-3 d-block px-2" href="#">
+        <img class="img-fluid" src="../../assets/trello.svg"/>
+      </a>
+      <a class="m-3 d-block px-2" href="#">
+        <img class="img-fluid" src="../../assets/git.svg"/>
+      </a>
+      <a class="m-3 d-block px-2" href="#">
+        <img class="img-fluid" src="../../assets/slack.svg"/>
+      </a>
+    </div>
+    </div>
+    <div class="row">
+      <div class="col-10">
+        <h4>Membros:</h4>
+        <div class="row">
+          <memberCard v-for="(index,member) in project.members" :key="index" :member-data="member"></memberCard>
+        </div>
+      </div>
+    </div>
+  </div>  
 </template>
-<script>
 
-export default {
-    data(){
-        return {
-            project: {}
-        }
+
+<script>
+  import memberCard from '../utils/MemberCard'
+
+  export default {
+
+    data() {
+      return {
+        project: {}
+      };
+    },
+    components:{
+      memberCard
     },
     async mounted() {
-        const projectRes = await this.axios.get(`${process.env.VUE_APP_API_URI}/project/${this.$route.params.id}`)
-        this.project = projectRes.data
-    }
-}
+      const projectRes = await this.axios.get(`project/${this.$route.params.id}`);
+      this.project = projectRes.data;
+    },
+  };
 </script>
