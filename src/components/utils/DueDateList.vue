a5a818423e72c7fd939c6cf654612a563e67c944
arthur-teixeira <40871498+arthur-teixeira@users.noreply.github.com>
2020-10-22 09:12:01 -0300

    collapse n ta funcionando aaaaaaaaaaaaaaaaaaaaa

--- src/components/utils/DueDateList.vue
+++ src/components/utils/DueDateList.vue
@@ -1,22 +1,35 @@
 <template>
   <div>
     <ol class="list-group my-2">
-      <form @submit.prevent="handleFormSubmit" class="p-2">
-       <button type="submit" class="btn btn-primary my-2 btn-lg">Adicionar</button>
-        <li v-for="(todo,index) in todos" :key="index" class="list-group-item">
-          <div class="form-group">
-            <label for="nome" class="sr-only">Nome</label>
-            <input type="text" v-model="todo.name" class="form-control" placeholder="nome" id="nome">
-          </div>
-          <div class="form-group">
-            <label for="date" class="sr-only">Data</label>
-            <input type="date" id="date" class="form-control" v-model="todo.date">
-          </div>
-          <div class="form-group">
-            <label for="description" class="sr-only">Descrição</label>
-            <input type="text" id="description" class="form-control" placeholder="Descrição" v-model="todo.description">
-          </div>
-        </li>
+      <form class="p-2">
+       <button type="submit"  @click.prevent="handleFormSubmit" class="btn btn-primary my-2 btn-lg">Adicionar</button>
+        <div id="accordion">
+          <li v-for="(todo,index) in todos" :key="index" class="list-group-item">
+            <div class="card">
+              <div class="card-header" :id="'h' + index">
+                <h5 class="mb-0">
+                  <button class="btn btn-link" type="button" data-toggle="collapse" :data-target="'#c' + index" :aria-expanded="setAriaExpanded(index)" :aria-controls="'c' + index">
+                    {{todo.name || 'nome'}}
+                  </button>
+                </h5>
+              </div>
+              <div :id="'c' + index" class="collapse" :class="setShowClass(index)" :aria-labelledby="'h' + index" data-parent="#accordion">
+                <div class="form-group" :id="'c' + index">
+                  <label for="nome" class="sr-only">Nome</label>
+                  <input type="text" v-model="todo.name" class="form-control" placeholder="nome" id="nome">
+                </div>
+                <div class="form-group">
+                  <label for="date" class="sr-only">Data</label>
+                  <input type="date" id="date" class="form-control" v-model="todo.date">
+                </div>
+                <div class="form-group">
+                  <label for="description" class="sr-only">Descrição</label>
+                  <input type="text" id="description" class="form-control" placeholder="Descrição" v-model="todo.description">
+                </div>
+              </div>
+            </div>
+          </li>
+        </div>
       </form>
     </ol>
   </div>
@@ -40,6 +53,12 @@ export default {
         description: this.description,
         isEditable : false
       });
+    },
+    setShowClass(index){
+      return index === 0 ? "show" : ""
+    },
+    setAriaExpanded(index){
+      return index === 0 ? "true" : "false"
     }
   }
 }
