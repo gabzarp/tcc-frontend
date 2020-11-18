8f2e1c596e927190579c29bd0e06da5fc12b78f2
Gabriel Zarpelon <zarpelon091098@gmail.com>
2020-11-18 19:03:03 -0300

    media no card de aceitação

--- src/components/utils/MemberCard.vue
+++ src/components/utils/MemberCard.vue
@@ -7,9 +7,12 @@
       <div class="col-8 text-light">
         <h5>{{memberData.user.name}}</h5>
         <p class="mb-1">{{memberData.position}}</p>
-        <router-link class="btn btn-primary" v-if="isScrumMaster && memberData.position != 'Scrum master'" :to="{path: `/project/${projectData._id}/member-evaluation/${memberData._id}` }"> 
-          Avaliar 
-        </router-link>
+          <div>Prazo: {{dueDate}}</div>
+          <div>Comunicação: {{communication}}</div>
+          <div>Conhecimento técnico: {{technicalKnowledge}}</div>
+          <router-link class="btn btn-primary" v-if="isScrumMaster && memberData.position != 'Scrum master'" :to="{path: `/project/${projectData._id}/member-evaluation/${memberData._id}` }"> 
+            Avaliar 
+          </router-link>
           <div v-if="isSolicitation" class="d-flex py-2">
             <a href="#" class="btn btn-success text-decoration-none mr-2" v-on:click="accept(memberData.user._id)" >Aceitar</a>
             <a href="#" class="btn btn-danger text-decoration-none">Ignorar</a>
@@ -22,11 +25,46 @@
 
 <script>
 export default {
+  data(){
+    return {
+      dueDate: '',
+      communication: '',
+      technicalKnowledge: ''
+    }
+  },
   props: ['isScrumMaster', 'projectData', 'memberData', 'isSolicitation'],
+  async mounted() {
+    var dueDate = this.memberData.evaluations.map(evaluation=>{
+    if(evaluation.type == '5f8cc48101fca84044fccbe9')
+        return evaluation.score
+    })
+    dueDate = dueDate.filter(evaluation=>{
+      return typeof evaluation !== 'undefined'
+    })
+    var communication = this.memberData.evaluations.map(evaluation=>{
+      if(evaluation.type == '5f8ce42b01fca84044fccbea')
+        return evaluation.score
+    })
+    communication = communication.filter(evaluation=>{
+      return typeof evaluation !== 'undefined'
+    })
+    var technicalKnowledge = this.memberData.evaluations.map(evaluation=>{
+      if(evaluation.type == '5f8ce43e01fca84044fccbeb')
+        return evaluation.score
+    })
+    technicalKnowledge = technicalKnowledge.filter(evaluation=>{
+      return typeof evaluation !== 'undefined'
+    })
+    this.dueDate = dueDate.reduce((a, b) => a + b, 0) / dueDate.length
+    this.communication = communication.reduce((a, b) => a + b, 0) / communication.length
+    this.technicalKnowledge = technicalKnowledge.reduce((a, b) => a + b, 0) / technicalKnowledge.length
+  },
   methods: {
       async accept(memberId) {
           try {
               await this.axios.post("/associate", {projectId: this.$route.params.id, userId: memberId});
+             
+              await this.axios.get("/associate", {projectId: this.$route.params.id, userId: memberId});
               this.$router.back();
           } catch (error) {
               this.$router.back();
